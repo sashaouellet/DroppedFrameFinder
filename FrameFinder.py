993b586726b66b9594db1677174601b7d0fa5398
spaouellet <spaouellet@omni-flybook.local>
2016-04-19 16:31:11 -0400

    Returns renderfarm friendly format for dropped frames

--- FrameFinder.py
+++ FrameFinder.py
@@ -34,7 +34,6 @@ def getSequence(entirePath):
 		if (re.match(compareRegex, file)):
 			sequence.append(file)
 
-	print sequence
 	return sequence
 
 #Builds regex based on the individual filename components. Substitutes wildcards for frame numbers so that we can match the entire rest of the file
@@ -48,8 +47,43 @@ def buildSequenceMatchRegex(fileNameList, sequenceNum):
 	regex = regex[:-2]
 	return regex
 
+def getFileSequenceAsNumbers(files):
+	sequence = []
+	for file in files:
+		sequenceNum = re.search('\.(\d+)', file).group(1)
+		sequence.append(int(sequenceNum.lstrip('0'))) #Remove leading 0's and append to the list
+
+	return sequence
+
+def getDroppedFrameNumbers(numberSequence):
+	firstFrame = min(numberSequence)
+	lastFrame = max(numberSequence)
+
+	firstConsecutive = firstFrame
+	consecutiveCount = 0;
+	
+	output = ""
+
+	for i in range(firstFrame, lastFrame + 1):
+		if i not in numberSequence:
+			if (consecutiveCount == 0): #If we haven't started a streak yet, set the start point to the current iteration
+				firstConsecutive = i
+				#Also append the beginning portion to our output string
+				output += ", " + str(i)
+			consecutiveCount += 1 #Add 1 to our streak
+		else:
+			if (consecutiveCount > 0):
+				#End consecutive streak
+				consecutiveCount = 0
+				#Reset starting point to current iteration
+				firstConsecutive = i
+				#Append end of range to output
+				output += "-" + str(i-1)
+
+	print output[2:]
+
 
 path = getPathFromUser()
-getSequence(path)
+getDroppedFrameNumbers(getFileSequenceAsNumbers(getSequence(path)))
 
 
