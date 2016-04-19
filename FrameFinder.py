a2976c372619de56014dea662dd2c0115b43ad80
spaouellet <spaouellet@omni-flybook.local>
2016-04-19 16:07:41 -0400

    Properly matches filenames to exact format of given seed file

--- FrameFinder.py
+++ FrameFinder.py
@@ -23,18 +23,31 @@ def getSequence(entirePath):
 	directoryFileList = re.split('(\/(\w+\/{1})+)', entirePath)
 	baseDirectory = directoryFileList[1]
 	fileName = directoryFileList[3]
-	print re.split('(\.\d+)', fileName)
-	fileRootName = re.split('(\.\d+)', fileName)[0]
-	sequenceNum = re.search('(\.\d+)', fileName).group(0).replace(".", "") #Finds sequence number by '.' and '#{x}' combination, strip the '.'
+	fileNameParts = fileName.split(".")
+	fileRootName = fileNameParts[0]
+	sequenceNum = re.search('\.(\d+)', fileName).group(1) #Finds sequence number by '.' and '#{x}' combination
 	directory = os.listdir(baseDirectory)
 	sequence = []
+	compareRegex = buildSequenceMatchRegex(fileNameParts, sequenceNum)
 
 	for file in directory:
-		if (file.startswith(fileRootName)):
+		if (re.match(compareRegex, file)):
 			sequence.append(file)
 
 	print sequence
 	return sequence
+
+#Builds regex based on the individual filename components. Substitutes wildcards for frame numbers so that we can match the entire rest of the file
+def buildSequenceMatchRegex(fileNameList, sequenceNum):
+	regex = ''
+	for part in fileNameList:
+		if part != sequenceNum:
+			regex += part + '\.'
+		else:
+			regex += '.' * len(sequenceNum) + '\.'
+	regex = regex[:-2]
+	return regex
+
 
 path = getPathFromUser()
 getSequence(path)
